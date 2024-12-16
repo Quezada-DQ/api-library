698c0b8c86b1ef39cf46d922e6d0d90eb9bd80fc
Quezada_DQ <Dahianquezada07@gmail.com>
2024-12-16 08:41:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const readData = () => {
+  try {
+    const data = fs.readFileSync("./db.json");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const writeData = (data) => {
+  try {
+    fs.writeFileSync("./db.json", JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Localhost running, library api data update 1");
+});
+
+app.get("/books", (req, res) => {
+  const data = readData();
+  res.json(data.books);
+});
+
+app.get("/books/:id", (req, res) => {
+  const data = readData();
+  const id = parseInt(req.params.id);
+  const book = data.books.find((book) => book.id === id);
+  res.json(book);
+});
+
+app.post("/books", (req, res) => {
+  const data = readData();
+  const body = req.body;
+  const newBook = {
+    id: data.books.length + 101,
+    ...body,
+  };
+  data.books.push(newBook);
+  writeData(data);
+  res.json(newBook);
+});
+
+app.put("/books/:id", (req, res) => {
+  const data = readData();
+  const body = req.body;
+  const id = parseInt(req.params.id);
+  const bookIndex = data.books.findIndex((book) => book.id === id);
+  data.books[bookIndex] = {
+    ...data.books[bookIndex],
+    ...body,
+  };
+  writeData(data);
+  res.json({ message: "Actualizacion de libro completa" });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const data = readData();
+  const id = parseInt(req.params.id);
+  const bookIndex = data.books.findIndex((book) => book.id === id);
+  data.books.splice(bookIndex, 1);
+  writeData(data);
+  res.json({ message: "Eliminacion de libro completa" });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
